a9eb000d72a1563180cc4a7fba40aecf6e8680c7
Melissa Preece <melissanchez@gmail.com>
2021-03-05 11:29:11 -0800

    Updates
    - create 404 error handler
    - test Book model and communicate with db
    - link stylesheet to layout
    - create page-not-found pug view

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -8,7 +8,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/book');
 
 // import Sequelize instance (access to all Sequelize methods and functionality)
-const sequelize = require('./models');
+const { sequelize } = require('./models');
 
 const app = express();
 
@@ -40,7 +40,11 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  // next(createError(404));
+  const error = new Error();
+  error.status = 404;
+  error.message = 'Sorry! We couldn"t find the page you were looking for.'
+  res.render('page-not-found', { error, message: error.message }) // pass {error} as 2nd parameter
 });
 
 // error handler
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const Book = require('../models').Book;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  
+  const books = await Book.findAll();
+  await res.json(books);
+  console.log(books);
 });
 
 module.exports = router;
